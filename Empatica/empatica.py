a49bf15958dca11e99614655348c46e558e964c6
P. H. <36509964+phelstab@users.noreply.github.com>
2024-07-29 15:55:30 +0200

    good

--- Empatica/empatica.py
+++ Empatica/empatica.py
@@ -1,60 +1,181 @@
+import sys
 import socket
+import csv
+import time
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
+from PySide6.QtCore import QThread, Signal
+import pyqtgraph as pg
 
 # E4 Streaming Server configuration
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 28000
 
-# Create a socket and connect to the E4 Streaming Server
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((SERVER_ADDRESS, SERVER_PORT))
+class DataReceiver(QThread):
+    data_received = Signal(str)
 
-# Function to send a command to the server
-def send_command(command):
-    sock.sendall((command + '\r\n').encode())
+    def run(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((SERVER_ADDRESS, SERVER_PORT))
 
-# Function to receive data from the server
-def receive_data():
-    return sock.recv(1024).decode('utf-8')
+        # Send commands to connect and subscribe to data streams
+        sock.sendall(b'device_list\r\n')
+        response = sock.recv(1024).decode('utf-8')
+        device_id = response.split('|')[1].strip().split(' ')[0]
 
-# Get the list of devices connected over BTLE
-send_command('device_list')
-response = receive_data()
-print(response)
+        sock.sendall(f'device_connect {device_id}\r\n'.encode())
+        sock.recv(1024)
 
-# Extract the device ID from the response
-device_id = response.split('|')[1].strip().split(' ')[0]
+        streams = ['acc', 'bvp', 'gsr', 'tmp', 'ibi', 'hr']
+        for stream in streams:
+            sock.sendall(f'device_subscribe {stream} ON\r\n'.encode())
+            sock.recv(1024)
 
-# Connect to the device
-send_command(f'device_connect {device_id}')
-response = receive_data()
-print(response)
+        while True:
+            data = sock.recv(1024).decode('utf-8').strip()
+            if data:
+                self.data_received.emit(data)
 
-# Subscribe to desired data streams
-send_command('device_subscribe acc ON')
-response = receive_data()
-print(response)
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Empatica E4 Data Plotter')
 
-send_command('device_subscribe bvp ON')
-response = receive_data()
-print(response)
+        layout = QVBoxLayout()
 
-send_command('device_subscribe gsr ON')
-response = receive_data()
-print(response)
+        self.acc_plot = pg.PlotWidget(title='Acceleration')
+        self.bvp_plot = pg.PlotWidget(title='Blood Volume Pulse')
+        self.gsr_plot = pg.PlotWidget(title='Galvanic Skin Response')
+        self.tmp_plot = pg.PlotWidget(title='Skin Temperature')
+        self.ibi_plot = pg.PlotWidget(title='Interbeat Interval')
+        self.hr_plot = pg.PlotWidget(title='Heart Rate')
 
-# # Continuously receive and process data from the device
-# try:
-#     while True:
-#         data = receive_data().strip()
-#         if data:
-#             print(data)
-# except KeyboardInterrupt:
-#     pass
+        layout.addWidget(self.acc_plot)
+        layout.addWidget(self.bvp_plot)
+        layout.addWidget(self.gsr_plot)
+        layout.addWidget(self.tmp_plot)
+        layout.addWidget(self.ibi_plot)
+        layout.addWidget(self.hr_plot)
 
-# Disconnect from the device
-send_command('device_disconnect')
-response = receive_data()
-print(response)
+        self.log_button = QPushButton('Start Logging')
+        self.log_button.clicked.connect(self.toggle_logging)
+        layout.addWidget(self.log_button)
 
-# Close the socket connection
-sock.close()
+        self.setLayout(layout)
+
+        self.acc_data = []
+        self.bvp_data = []
+        self.gsr_data = []
+        self.tmp_data = []
+        self.ibi_data = []
+        self.hr_data = []
+
+        self.acc_curve = self.acc_plot.plot()
+        self.bvp_curve = self.bvp_plot.plot()
+        self.gsr_curve = self.gsr_plot.plot()
+        self.tmp_curve = self.tmp_plot.plot()
+        self.ibi_curve = self.ibi_plot.plot()
+        self.hr_curve = self.hr_plot.plot()
+
+        self.logging_enabled = False
+        self.acc_file = None
+        self.bvp_file = None
+        self.gsr_file = None
+        self.tmp_file = None
+        self.ibi_file = None
+        self.hr_file = None
+        self.acc_writer = None
+        self.bvp_writer = None
+        self.gsr_writer = None
+        self.tmp_writer = None
+        self.ibi_writer = None
+        self.hr_writer = None
+
+        self.data_receiver = DataReceiver()
+        self.data_receiver.data_received.connect(self.update_plot)
+        self.data_receiver.start()
+
+    def update_plot(self, data):
+        stream_type, timestamp, *values = data.split()
+        timestamp = float(timestamp)
+        nanoseconds = int(timestamp * 1e9)
+
+        if stream_type == 'E4_Acc':
+            if len(values) == 3:
+                x, y, z = map(float, values)
+                self.acc_data.append((timestamp, x, y, z))
+                self.acc_curve.setData([d[0] for d in self.acc_data[-100:]], [d[1] for d in self.acc_data[-100:]])
+                if self.logging_enabled:
+                    self.acc_writer.writerow([nanoseconds, x, y, z])
+                    self.acc_file.flush()
+
+        elif stream_type == 'E4_Bvp':
+            value = float(values[0])
+            self.bvp_data.append((timestamp, value))
+            self.bvp_curve.setData([d[0] for d in self.bvp_data[-100:]], [d[1] for d in self.bvp_data[-100:]])
+            if self.logging_enabled:
+                self.bvp_writer.writerow([nanoseconds, value])
+                self.bvp_file.flush()
+
+        elif stream_type == 'E4_Gsr':
+            value = float(values[0])
+            self.gsr_data.append((timestamp, value))
+            self.gsr_curve.setData([d[0] for d in self.gsr_data[-100:]], [d[1] for d in self.gsr_data[-100:]])
+            if self.logging_enabled:
+                self.gsr_writer.writerow([nanoseconds, value])
+                self.gsr_file.flush()
+
+        elif stream_type == 'E4_Temperature':
+            value = float(values[0])
+            self.tmp_data.append((timestamp, value))
+            self.tmp_curve.setData([d[0] for d in self.tmp_data[-100:]], [d[1] for d in self.tmp_data[-100:]])
+            if self.logging_enabled:
+                self.tmp_writer.writerow([nanoseconds, value])
+                self.tmp_file.flush()
+
+        elif stream_type == 'E4_Ibi':
+            value = float(values[0])
+            self.ibi_data.append((timestamp, value))
+            self.ibi_curve.setData([d[0] for d in self.ibi_data[-100:]], [d[1] for d in self.ibi_data[-100:]])
+            if self.logging_enabled:
+                self.ibi_writer.writerow([nanoseconds, value])
+                self.ibi_file.flush()
+
+        elif stream_type == 'E4_Hr':
+            value = float(values[0])
+            self.hr_data.append((timestamp, value))
+            self.hr_curve.setData([d[0] for d in self.hr_data[-100:]], [d[1] for d in self.hr_data[-100:]])
+            if self.logging_enabled:
+                self.hr_writer.writerow([nanoseconds, value])
+                self.hr_file.flush()
+
+    def toggle_logging(self):
+        if not self.logging_enabled:
+            self.acc_file = open('acc_data.csv', 'w', newline='')
+            self.bvp_file = open('bvp_data.csv', 'w', newline='')
+            self.gsr_file = open('gsr_data.csv', 'w', newline='')
+            self.tmp_file = open('tmp_data.csv', 'w', newline='')
+            self.ibi_file = open('ibi_data.csv', 'w', newline='')
+            self.hr_file = open('hr_data.csv', 'w', newline='')
+            self.acc_writer = csv.writer(self.acc_file)
+            self.bvp_writer = csv.writer(self.bvp_file)
+            self.gsr_writer = csv.writer(self.gsr_file)
+            self.tmp_writer = csv.writer(self.tmp_file)
+            self.ibi_writer = csv.writer(self.ibi_file)
+            self.hr_writer = csv.writer(self.hr_file)
+            self.log_button.setText('Stop Logging')
+            self.logging_enabled = True
+        else:
+            self.acc_file.close()
+            self.bvp_file.close()
+            self.gsr_file.close()
+            self.tmp_file.close()
+            self.ibi_file.close()
+            self.hr_file.close()
+            self.log_button.setText('Start Logging')
+            self.logging_enabled = False
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
